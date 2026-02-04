d6823f8dff0c198ce1db525ce0733ff50eb79ca3
massimo-ruggiero <ruggieromssm@gmail.com>
2026-02-05 00:28:30 +0100

    fixed erorrs

--- models/dino_v2.py
+++ models/dino_v2.py
@@ -6,7 +6,8 @@ import timm
 class DinoV2Timm(nn.Module):
     def __init__(self, 
                  model_name="vit_base_patch14_dinov2", 
-                 pretrained=True): 
+                 pretrained=True,
+                 freeze_backbone=True): 
         super().__init__()
         self.backbone = timm.create_model(
             model_name,
@@ -15,10 +16,10 @@ class DinoV2Timm(nn.Module):
         )
         self.embed_dim = self.backbone.num_features
 
-        for param in self.backbone.parameters():
-            param.requires_grad = False
-
-        self.backbone.eval()
+        if freeze_backbone:
+            for param in self.backbone.parameters():
+                param.requires_grad = False
+            self.backbone.eval()
 
     def forward_features(self, x):
         with torch.no_grad():
